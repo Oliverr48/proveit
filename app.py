bcc20d92cb6d61567808298ab8a74b46219c61b4
Sam Dockery <samdockery04@gmail.com>
2025-05-01 08:53:02 +0800

    Incorporated Project and Task Tables into models.py and integrated HTML templates into the dashboard and project pages. Added requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from website import create_app, db
-from website.models import Project
+from website.models import Project, Task
 
 print("🔍 app.py is being run — __name__ =", __name__)
 
@@ -10,11 +10,39 @@ if __name__ == '__main__':
         db.create_all()
 
         #used this to make some sample data - just check it's not already there if you're running it again, otherwise the app fails. 
-        if not Project.query.filter_by(name='Project1').first():
+        if not Project.query.filter_by(name='Project 1').first():
             db.session.add(Project(
             name='Project 1',
             description='Make something blah blah blah',
-            dueDate='2023-10-01'
+            dueDate='2023-10-01',
+            tasksActive=4,
+            tasksCompleted=1
+         ))
+            db.session.commit()
+            print("Project added!")
+        else:
+            print("[!] Project already exists!")
+        
+        if not Task.query.filter_by(name='Task 1 for Project 1').first():
+            db.session.add(Task(
+            parentProject=1,
+            name='Task 1 for Project 1',
+            description='Specific task',
+            dueDate='2023-10-01',
+            status=1
+         ))
+            db.session.commit()
+            print("Project added!")
+        else:
+            print("[!] Project already exists!")
+
+        if not Task.query.filter_by(name='Task 2 for Project 1').first():
+            db.session.add(Task(
+            parentProject=1,
+            name='Task 2 for Project 1',
+            description='Specific task',
+            dueDate='2023-10-01',
+            status=0
          ))
             db.session.commit()
             print("Project added!")
--- website/routes.py
+++ website/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from .models import Project
+from .models import Project, Task
 from flask_login import login_required, current_user
 
 routes = Blueprint('routes', __name__)
@@ -11,12 +11,18 @@ def index():
 @routes.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', user=current_user)
+    projects = Project.query.all()
+    comTasks = Task.query.filter_by(status=1)
+    totalTasks = Task.query.all()
+    return render_template('dashboard.html', user=current_user, projects=projects, comTasks=comTasks, totalTasks=totalTasks)
 
 @routes.route('/projects')
 @login_required
 def projects():
-    # Get the projects from the DB 
+    #Get the projs from the DB 
     projects = Project.query.all()
+    # May need to update this in future to get tasks only for active projects? 
+    comTasks = Task.query.filter_by(status=1)
     print("Got the projects!")
-    return render_template('project.html', projects=projects, user=current_user)
+    return render_template('project.html', projects=projects, comTasks=comTasks, user=current_user)
+
