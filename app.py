214c064bfec46c46cc9fa5e39ca58aff7757bfbd
Sam Dockery <samdockery04@gmail.com>
2025-05-04 10:45:29 +0800

    Worked New Project Form Modal into the Project database table! Added link to a new project_view template page for individual project View Buttons, where the project tasks etc can be displayed.

--- app.py
+++ app.py
@@ -10,43 +10,6 @@ if __name__ == '__main__':
         db.create_all()
 
         #used this to make some sample data - just check it's not already there if you're running it again, otherwise the app fails. 
-        if not Project.query.filter_by(name='Project 1').first():
-            db.session.add(Project(
-            name='Project 1',
-            description='Make something blah blah blah',
-            dueDate='2023-10-01',
-            tasksActive=4,
-            tasksCompleted=1
-         ))
-            db.session.commit()
-            print("Project added!")
-        else:
-            print("[!] Project already exists!")
-        
-        if not Task.query.filter_by(name='Task 1 for Project 1').first():
-            db.session.add(Task(
-            parentProject=1,
-            name='Task 1 for Project 1',
-            description='Specific task',
-            dueDate='2023-10-01',
-            status=1
-         ))
-            db.session.commit()
-            print("Project added!")
-        else:
-            print("[!] Project already exists!")
-
-        if not Task.query.filter_by(name='Task 2 for Project 1').first():
-            db.session.add(Task(
-            parentProject=1,
-            name='Task 2 for Project 1',
-            description='Specific task',
-            dueDate='2023-10-01',
-            status=0
-         ))
-            db.session.commit()
-            print("Project added!")
-        else:
-            print("[!] Project already exists!")
+       
 
     app.run(debug=True)
--- website/routes.py
+++ website/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from .models import Project, Task
 from flask_login import login_required, current_user
+from . import db # Import the db object
 
 routes = Blueprint('routes', __name__)
 
@@ -25,4 +26,34 @@ def projects():
     comTasks = Task.query.filter_by(status=1)
     print("Got the projects!")
     return render_template('project.html', projects=projects, comTasks=comTasks, user=current_user)
+
+@routes.route('/submitNewProject', methods=['POST'])
+def submitNewProject():
+    print("Are we here???")
+    # Get the form data
+    name = request.form['projectName']
+    description = request.form['projectDescription']
+    dueDate = request.form['projectDueDate']
+    tasksActive = 0  # Assuming you want to initialize this to 0 when creating a new project
+    tasksCompleted = 0  # Assuming you want to initialize this to 0 when creating a new project
+    # Create a new project instance
+    new_project = Project(
+        name=name,
+        description=description,
+        dueDate=dueDate,
+        tasksActive=tasksActive,
+        tasksCompleted=tasksCompleted
+    )
+
+    # Add the new project to the database session and commit
+    db.session.add(new_project)
+    db.session.commit()
+
+    return jsonify({'message': 'Project created successfully'})
+
+@routes.route('/project_view/<int:project_id>')
+def project_view(project_id):
+    project = Project.query.get_or_404(project_id)
+    tasks = Task.query.filter_by(parentProject=project_id).all()
+    return render_template('project_view.html', project=project, tasks=tasks, user=current_user)
 
