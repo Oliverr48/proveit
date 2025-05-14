8cf1e7b1765705c20fc8bbb97b235a06c22d5310
Michelle <michelle>
2025-05-15 00:36:50 +0800

    Changed profile name to user's first and last name

--- website/models.py
+++ website/models.py
@@ -65,7 +65,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), nullable=False, unique=True)
 
     def __str__(self):
-        return f"User: {self.username}, Email: {self.email}"
+        return f"User: {self.firstName} {self.lastName}, Username: {self.username}, Email: {self.email}"
 
 class Activity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
